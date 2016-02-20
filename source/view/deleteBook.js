a60bfbcedc5174ea806685342d181c091548a9c4
James Valaitis <james@infinityjames.com>
2016-02-20 14:14:24 +0000

    Implemented ability to delete a book.

---
+++ source/view/deleteBook.js
@@ -0,0 +1,35 @@
+publiclibrary.view.deleteBook = {
+  setupUserInterface: function() {
+    var deleteButton = document.forms['Book'].commit;
+    var selectElement = document.forms['Book'].selectBook;
+    var index = 0, key = "", keys = [], book = null, optionElement = null;
+
+    //  load all book objects
+    Book.loadAll();
+    keys = Object.keys(Book.instances);
+
+    //  populate te select list with books
+    for (index = 0; index < keys.length; index++) {
+      key = keys[index];
+      book = Book.instances[key];
+      optionElement = document.createElement("option");
+      optionElement.text = book.title;
+      optionElement.value = book.isbn;
+      selectElement.add(optionElement, null);
+    }
+
+    deleteButton.addEventListener("click", 
+                publiclibrary.view.deleteBook.handleDeleteButtonClickEvent);
+    window.addEventListener("beforeunload", function() {
+      Book.saveAll();
+    });
+  },
+  handleDeleteButtonClickEvent: function() {
+    var selectElement = document.forms['Book'].selectBook;
+    var isbn = selectElement.value;
+    if (isbn) {
+      Book.destroy(isbn);
+      selectElement.remove(selectElement.selectedIndex);
+    }
+  }
+};
